efd693a1979e2588559e0b9a817dd54d72a64ff4
David-MacD <149108133+David-MacD@users.noreply.github.com>
2024-05-09 17:41:09 +0100

    Parameterise the json location in the services route

--- src/app/api/services/route.ts
+++ src/app/api/services/route.ts
@@ -6,11 +6,13 @@ import {
   tryParseJson,
 } from "@/lib/util";
 
+const jsonLocation = "./data/mockBackend.json";
+
 export const GET = async (req: Request) => {
   const headers = { "Content-Type": "application/json" };
 
   try {
-    const data = await fs.readFile("./data/mockBackend.json", "utf-8");
+    const data = await fs.readFile(jsonLocation, "utf-8");
     const services = tryParseJson<Service[]>(data, isServicesArray);
     if (services) {
       return new Response(makeResponseBody("ok", services), {
