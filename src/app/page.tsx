c30d505a0a618e829f6404053e5793ed8b6da8c7
teonkajkafon <catherinesarquis@gmail.com>
2024-05-10 06:02:10 +0100

    update services list to reflect mock backend data to include timestamp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,30 +2,16 @@ import React from "react";
 import { ServicesContainer } from "@/components/ServiceHealth";
 
 const services = [
-  {
-    name: "Netlify",
-    healthy: false,
-  },
-  {
-    name: "Twilio",
-    healthy: true,
-  },
-  {
-    name: "Sentry",
-    healthy: false,
-  },
-  // {
-  //   name: "Azure",
-  //   healthy: true,
-  // },
-  // {
-  //   name: "Supabase",
-  //   healthy: false,
-  // },
-  // {
-  //   name: "Clerk",
-  //   healthy: false,
-  // },
+  { name: "netlify", timestamp: 1715272492681, healthy: true },
+  { name: "auth0", timestamp: 1722812400000, healthy: false },
+  { name: "twilio", timestamp: 1725490800000, healthy: true },
+  { name: "netlify", timestamp: 1715272492681, healthy: true },
+  { name: "auth0", timestamp: 1722812400000, healthy: false },
+  { name: "netlify", timestamp: 1715272492681, healthy: true },
+  { name: "auth0", timestamp: 1722812400000, healthy: false },
+  { name: "twilio", timestamp: 1725490800000, healthy: true },
+  { name: "netlify", timestamp: 1715272492681, healthy: true },
+  { name: "auth0", timestamp: 1722812400000, healthy: false },
 ];
 
 export default function Home() {
