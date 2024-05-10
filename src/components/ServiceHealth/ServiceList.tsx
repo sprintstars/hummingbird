f387927a9ea3b4aa1770caa50e27a9797b5ea7c8
teonkajkafon <catherinesarquis@gmail.com>
2024-05-10 06:08:39 +0100

    add timestamp to ServiceStatus type and to ServiceHealth component attributes and remove shadow-ib from ServiceList as it scrolls

--- src/components/ServiceHealth/ServiceList.tsx
+++ src/components/ServiceHealth/ServiceList.tsx
@@ -3,6 +3,7 @@ import ServiceHealth from "./ServiceHealth";
 //specifically type things at top level
 export type ServiceStatus = {
   name: string;
+  timestamp: number;
   healthy: boolean;
 };
 
@@ -12,11 +13,12 @@ type ServiceListProps = {
 
 const ServiceList = (props: Readonly<ServiceListProps>) => {
   return (
-    <div className="flex flex-col px-6 gap-3 h-full items-center shadow-ib self-center min-w-80">
+    <div className="flex flex-col px-6 gap-6 h-full items-center self-center min-w-80">
       {props.list.map((service, i) => (
         <ServiceHealth
           key={service.name + i}
           name={service.name}
+          timestamp={service.timestamp}
           healthy={service.healthy}
         />
       ))}
