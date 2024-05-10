7e23c75d68ef8a2278d8d0b3e901a4db05dd4d92
rikharthulee <rikharthulee@gmail.com>
2024-05-10 12:10:28 +0100

    make frontend call data from backend

--- src/components/ServiceHealth/ServicesContainer.tsx
+++ src/components/ServiceHealth/ServicesContainer.tsx
@@ -1,13 +1,14 @@
 import ServiceList, { type ServiceStatus } from "./ServiceList";
+import { type Service } from "@/lib/util";
 
-type ServiceContainerProps = {
-  list: ServiceStatus[];
-};
+const ServicesContainer = async () => {
+  const response = await fetch("http://localhost:3000/api/services/");
+  const body = await response.json();
+  const services: Service[] = body.payload;
 
-const ServicesContainer = (props: Readonly<ServiceContainerProps>) => {
-  const disruptions = props.list.reduce(
-    (sum, service) => service.healthy ? sum : sum + 1,
-    0,
+  const disruptions = services.reduce(
+    (sum, service) => (service.healthy ? sum : sum + 1),
+    0
   );
 
   return (
@@ -16,21 +17,19 @@ const ServicesContainer = (props: Readonly<ServiceContainerProps>) => {
         Ordering & Filtering
       </div>
       <div className="col-start-1 col-span-6 md:col-span-3 row-start-5 row-span-1 md:row-start-3 md:row-span-4">
-        <ServiceList list={props.list} />
+        <ServiceList list={services} />
       </div>
       {/* Details */}
       <div className="grid grid-cols-[3fr_1fr] text-right mx-auto md:grid-cols-2 col-start-1 col-span-6 md:col-start-4 md:col-span-3 gap-[5vh] row-start-3 row-span-2 md:row-start-3 md:row-span-3 px-10 content-around text-3xl text-slate-200">
         <span>Disruptions</span>
-        <span className="flex items-center">
-          {disruptions}
-        </span>
+        <span className="flex items-center">{disruptions}</span>
         <span>Recently Resolved</span>
         <span className="flex items-center">0</span>
         <span>Fully Operational</span>
         <span className="flex items-center">
-          {props.list.reduce(
-            (sum, service) => service.healthy ? sum + 1 : sum,
-            0,
+          {services.reduce(
+            (sum, service) => (service.healthy ? sum + 1 : sum),
+            0
           )}
         </span>
       </div>
