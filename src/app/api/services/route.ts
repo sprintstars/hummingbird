62a0d0d2ac7f76146e4b52f311d2f0fae1bff5cb
David-MacD <149108133+David-MacD@users.noreply.github.com>
2024-05-10 10:42:36 +0100

    Flip the error branch and explicitly test for null

--- src/app/api/services/route.ts
+++ src/app/api/services/route.ts
@@ -13,16 +13,16 @@ export const GET = async (req: Request) => {
   try {
     const data = await fs.readFile(jsonLocation, "utf-8");
     const services = tryParseJson<Service[]>(data, isServicesArray);
-    if (services) {
-      return new Response(makeResponseBody("ok", services), { headers });
+    if (services === null) {
+      return new Response(
+        makeResponseBody(
+          "error",
+          "There was a problem validating the data from the database",
+        ),
+        { headers },
+      );
     }
-    return new Response(
-      makeResponseBody(
-        "error",
-        "There was a problem validating the data from the database",
-      ),
-      { headers },
-    );
+    return new Response(makeResponseBody("ok", services), { headers });
   } catch (e) {
     const message = e instanceof Error ? e.message : "Oops!";
     return new Response(makeResponseBody("error", message), { headers });
