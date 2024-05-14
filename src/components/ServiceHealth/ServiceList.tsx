c312c55d4db8ab5bee0b0017e241d5dc4e016ec5
teonkajkafon <catherinesarquis@gmail.com>
2024-05-14 13:49:04 +0100

    sorted the props.list to display unhealthy first

--- src/components/ServiceHealth/ServiceList.tsx
+++ src/components/ServiceHealth/ServiceList.tsx
@@ -9,6 +9,7 @@ const ServiceList = (props: Readonly<ServiceListProps>) => {
   const sortedList = [...props.list].sort((a, b) =>
     a.healthy === b.healthy ? 0 : a.healthy ? 1 : -1
   );
+  //what is going on
 
   return (
     <div className="flex flex-col w-[25rem] gap-3 h-full mx-auto items-center overflow-y-auto">
