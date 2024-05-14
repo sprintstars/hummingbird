d918c43441adf4b998af11d77f0d9d6922d3ddf5
teonkajkafon <catherinesarquis@gmail.com>
2024-05-14 14:02:13 +0100

    expand sortedlist function and add comments

--- src/components/ServiceHealth/ServiceList.tsx
+++ src/components/ServiceHealth/ServiceList.tsx
@@ -6,10 +6,21 @@ type ServiceListProps = {
 };
 
 const ServiceList = (props: Readonly<ServiceListProps>) => {
-  const sortedList = [...props.list].sort((a, b) =>
-    a.healthy === b.healthy ? 0 : a.healthy ? 1 : -1
-  );
-  //what is going on
+  const sortedList = [...props.list].sort((a, b) => {
+    if (a.healthy === b.healthy) {
+      // If they do, they are considered equal in terms of sorting
+      return 0;
+    } else {
+      // If 'a' is healthy and 'b' is not, 'a' should come after 'b'
+      if (a.healthy) {
+        return 1;
+      }
+      // If 'b' is healthy and 'a' is not, 'a' should come before 'b'
+      else {
+        return -1;
+      }
+    }
+  });
 
   return (
     <div className="flex flex-col w-[25rem] gap-3 h-full mx-auto items-center overflow-y-auto">
