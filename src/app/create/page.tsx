0f42349bd1eff3a2fd1fceaef5712015d0aeb98c
rikharthulee <rikharthulee@gmail.com>
2024-05-21 14:32:36 +0100

    Form designed and formatted

---
+++ src/app/create/page.tsx
@@ -0,0 +1,145 @@
+import { Input } from "@/components/Primitives/Input";
+import { createClient } from "@/lib/supabase/server";
+import db from "@/lib/db";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+  SubmitButton,
+} from "@/components/Primitives";
+import { redirect } from "next/navigation";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/Primitives/Dialog";
+
+export default function AddService({ searchParams }: { searchParams: { message: string } }) {
+  // Function to handle form submission
+
+  const createService = async (formData: FormData) => {
+    "use server";
+
+    const supabase = createClient();
+
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+
+    const serviceName = formData.get("name") as string;
+    const url = formData.get("url") as string;
+    const strategy = formData.get("strategy") as string;
+
+    if (serviceName && url && strategy && user) {
+      const response = await db.query("SELECT id, url from services WHERE url = $1", [url]);
+
+      // if response.rows is empty then add the service
+      if (response.rowCount === 0) {
+        const newServiceResponse = await db.query(
+          "INSERT INTO services (name, url, strategy) VALUES ($1, $2, $3) RETURNING *",
+          [serviceName, url, strategy]
+        );
+
+        // get the id of the newly added service and add it to the user
+
+        await db.query("INSERT INTO status_owners (service_id, user_id) VALUES ($1, $2)", [
+          newServiceResponse.rows[0].id,
+          user.id,
+        ]);
+      } else {
+        // status_owners 2 cols - service_id user_id
+        const statusOwnersResponse = await db.query(
+          "SELECT service_id, user_id FROM status_owners WHERE service_id = $1 AND user_id = $2",
+          [response.rows[0].id, user.id]
+        );
+
+        // if they aren't linked, link them
+
+        if (statusOwnersResponse.rowCount === 0) {
+          //link them
+          await db.query("INSERT INTO status_owners (service_id, user_id) VALUES ($1, $2)", [
+            response.rows[0].id,
+            user.id,
+          ]);
+        } else {
+          console.log("Already linked");
+          // if they are linked - service already linked and available to the user
+        }
+      }
+    }
+    if (Error()) {
+      return redirect("/?message=Could not add service");
+    }
+    return redirect("/status");
+  };
+  return (
+    <div className="bg-cover col-start-1 col-span-6 row-start-3 row-span-3 mx-auto flex flex-col w-full px-8 sm:max-w-md justify-center gap-6">
+      <h1 className="text-2xl font-bold mb-4">Add a new service</h1>
+      <form className="flex flex-col gap-4" action={createService}>
+        <div className="flex flex-col gap-1">
+          <label className="text-md font-medium" htmlFor="name">
+            Name (e.g. Twillio, AWS ...)
+          </label>
+          <Input type="text" name="name" id="name" placeholder="Service Name" required />
+        </div>
+
+        <div className="flex flex-col gap-1">
+          <label className="text-md font-medium" htmlFor="url">
+            URL
+          </label>
+          <Input type="text" name="url" id="url" placeholder="https://example.com" required />
+        </div>
+
+        <div className="flex flex-col gap-1">
+          <label className="text-md font-medium" htmlFor="strategy" aria-required>
+            Strategy
+          </label>
+
+          <Select name="strategy">
+            <SelectTrigger>
+              <SelectValue placeholder="Strategy" />
+            </SelectTrigger>
+            <SelectContent className="bg-background">
+              <SelectItem
+                className="cursor-pointer hover:bg-foreground hover:text-background"
+                value="ping"
+              >
+                Ping
+              </SelectItem>
+              <SelectItem
+                className="cursor-pointer hover:bg-foreground hover:text-background"
+                value="rss"
+              >
+                RSS
+              </SelectItem>
+              <SelectItem
+                className="cursor-pointer hover:bg-foreground hover:text-background"
+                value="json"
+              >
+                JSON
+              </SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+
+        <SubmitButton
+          formAction={createService}
+          className="bg-green-700 text-white rounded-md px-4 py-2 mt-4 hover:bg-green-800"
+          pendingText="Adding Service..."
+        >
+          Add service
+        </SubmitButton>
+        {searchParams.message && (
+          <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
+            {searchParams.message}
+          </p>
+        )}
+      </form>
+    </div>
+  );
+}
