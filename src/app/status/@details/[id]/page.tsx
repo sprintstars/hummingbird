0f5361883516634aa033e4d2bced747cd8ebdb40
David-MacD <149108133+David-MacD@users.noreply.github.com>
2024-05-26 17:31:08 +0100

    Fix the seed data for status_history and change how the query works
    - The query gets the percentage of downtime over the past month, to help populate the bar chart in status/@details/[id]

--- src/app/status/@details/[id]/page.tsx
+++ src/app/status/@details/[id]/page.tsx
@@ -8,20 +8,24 @@ const StatusDetailsView = async ({ params }: { params: { id: string } }) => {
   const response = await fetch(`${API}/services/${params.id}`, { cache: "no-cache" });
   const dailyResponse = await db.query(
     `
-      WITH cte AS (
-        SELECT
-          extract('day' from "time") AS day,
-          COUNT(CASE WHEN NOT healthy THEN 1 END) AS false_count,
-          COUNT(*) AS total_count
-        FROM status_history
-        WHERE "time" >= current_date - interval '29 days' AND service_id = $1
-        GROUP BY extract('day' from "time")
-      )
-      SELECT
-        day,
-        round(100.0 * false_count / total_count, 2) AS percentage
-      FROM cte
-      ORDER BY day;
+    with counts as (
+      select
+        to_char(status_history.time, 'MM-DD') "day",
+        count(CASE WHEN NOT healthy THEN 1 END) AS false_count,
+        count(*) AS total_count
+      from
+        status_history
+      where
+        status_history.time > current_timestamp - interval '28 days' AND service_id = $1
+      group by
+        1
+      order by
+        1
+    )
+    select
+      day,
+      round(100.0 * false_count / total_count, 2) AS percentage
+    from counts
     `,
     [params.id]
   );
